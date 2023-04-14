104cf3514b5fd640bb2ed0ca182bb69e8b7a3468
YASH TAILOR <theyash.tailor@gmail.com>
2023-04-14 12:56:28 +0530

    few changes made

--- src/com/yash/finalKeyword/Main.java
+++ src/com/yash/finalKeyword/Main.java
@@ -2,15 +2,19 @@ package com.yash.finalKeyword;
 
 import com.yash.finalKeyword.company.Employee;
 
-public class Main {
+public class Main extends Employee {
     public static void main(String[] args) {
         Employee emp = new Employee();
         emp.employeeDetails();
 
 //        if we try to re-assign value of final instance variable - throws an error
-//    error-message    java: cannot assign a value to final variable EMPLOYEE_ID
+//        error-message    java: cannot assign a value to final variable EMPLOYEE_ID
+//
 //        emp.EMPLOYEE_ID = 12232
-//        emp.employeeDetails();
 
+//        we try to override final method gives an error - illegal start of expression
+//        public final void employeeDetails(){
+//            System.out.println("method override");
+//        }
     }
 }
